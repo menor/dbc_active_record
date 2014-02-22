e0f98c49b6a45f2b9f26a73f4b046cc310762503
menor <josemenorgmail.com>
2014-02-22 11:48:42 -0800

    Migration is not working

--- source/ar-people-skills/db/migrate/20140222111218_create_userskills.rb
+++ source/ar-people-skills/db/migrate/20140222111218_create_userskills.rb
@@ -1,7 +1,7 @@
 class CreateUserSkills < ActiveRecord::Migration
   def change
-    t.integer :user_id
-    t.integer :skill_id
+    t.belongs_to :user_id
+    t.belongs_to :skill_id
     t.integer :rating
   end
 end
